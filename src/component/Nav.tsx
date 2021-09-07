167e8aed19ba80e5ddffa46ab4ddbb34b651ffbc
woojin-ahn <58835065+woojin-ahn@users.noreply.github.com>
2021-09-07 23:58:04 +0900

    dropdown 오른쪽으로 옮김

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,9 +1,8 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Row, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logoimg from './images/logo.png'
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 
 function Navigation(){
@@ -11,22 +10,19 @@ function Navigation(){
         return (
             <Navbar collapseOnSelect fixed='top' bg="info" variant='dark'>
                 <Container>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav"> </Navbar.Collapse>
-                    <Nav className="me-auto">
-                    <Navbar.Brand><img
+                    <Navbar.Brand href="/"><img
                         src={logoimg}
                         width="30"
                         height="30"
                         className="d-inline-block align-top"
                         alt="TodoApp logo"
-                    /></Navbar.Brand>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    </NavDropdown>
+                    />  TodoApp</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <NavDropdown title="List" id="basic-nav-dropdown">
+                        </NavDropdown>
                     </Nav>
                 </Container>
             </Navbar>
         )
 }
-
 export default Navigation;
